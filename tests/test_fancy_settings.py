c26aeb1d7eda6ea14c71c31e9e33a81793819b36
thom <thom@localhost.localdomain>
2018-04-09 18:12:25 -0400

    Added more tests.

--- tests/test_fancy_settings.py
+++ tests/test_fancy_settings.py
@@ -8,7 +8,7 @@ class TestFancySettings(TestCase):
     def setUp(self):
         self.view = sublime.active_window().new_file()
         self.settings = self.view.settings()
-        self.settings.set("example_setting", "Hello, World!")
+        self.fancy = FancySettings(self.settings)
 
     def tearDown(self):
         if self.view:
@@ -16,7 +16,35 @@ class TestFancySettings(TestCase):
             self.view.window().focus_view(self.view)
             self.view.window().run_command("close_file")
 
-    def test_get(self):
-        fancy = FancySettings(self.settings)
 
-        self.assertEqual(fancy['example_setting'], "Hello, World!")
+    def test_item(self):
+        self.settings.set("example_setting", "Hello, World!")
+
+        self.assertEqual(self.fancy['example_setting'], "Hello, World!")
+
+    def test_item_missing_error(self):
+        self.settings.erase("example_setting")
+
+        self.assertRaises(KeyError, lambda k: self.fancy[k], "example_setting")
+
+
+    def test_get(self):
+        self.settings.set("example_setting", "Hello, World!")
+
+        self.assertEqual(self.fancy.get('example_setting'), "Hello, World!")
+
+    def test_get_missing_none(self):
+        self.settings.erase("example_setting")
+
+        self.assertIsNone(self.fancy.get("example_setting"))
+
+    def test_get_missing_default(self):
+        self.settings.erase("example_setting")
+
+        self.assertEqual(self.fancy.get("example_setting", "default"), "default")
+
+
+    def test_set(self):
+        self.fancy["example_setting"] = "Hello, World!"
+
+        self.assertEqual(self.settings.get('example_setting'), "Hello, World!")
