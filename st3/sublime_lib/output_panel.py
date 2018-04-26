efc5133fe661b4586bb42d2fcb89f58ee37005be
thom <thom@localhost.localdomain>
2018-04-26 17:24:25 -0400

    Added new features to OutputPanel.

--- st3/sublime_lib/output_panel.py
+++ st3/sublime_lib/output_panel.py
@@ -6,12 +6,13 @@ class OutputPanel(ViewStream):
     def __init__(
         self, window, name, *,
         force_writes=False,
+        unlisted=False,
         **kwargs
     ):
         validate_view_options(kwargs)
 
         super().__init__(
-            window.get_output_panel(name),
+            window.create_output_panel(name, unlisted),
             force_writes=force_writes
         )
 
@@ -25,12 +26,23 @@ class OutputPanel(ViewStream):
         return "output.%s" % self.name
 
     @ViewStream.guard_validity
+    def is_visible(self):
+        return self.window.active_panel() == self.full_name
+
+    @ViewStream.guard_validity
     def show(self):
         self.window.run_command("show_panel", {"panel": self.full_name})
 
     @ViewStream.guard_validity
     def hide(self):
         self.window.run_command("hide_panel", {"panel": self.full_name})
+
+    @ViewStream.guard_validity
+    def toggle_visibility(self):
+        if self.is_visible():
+            self.hide()
+        else:
+            self.show()
 
     def destroy(self):
         self.window.destroy_output_panel(self.name)
