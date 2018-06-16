51801acaedfca00355b7f2bb53a53aa507dc3c93
Thomas Smith <thom@Thomass-iMac.lan>
2018-06-16 13:30:04 -0400

    Remove DefaultSettingsDict

--- st3/sublime_lib/__init__.py
+++ st3/sublime_lib/__init__.py
@@ -1,6 +1,5 @@
 from .settings_dict import SettingsDict  # noqa: F401
 from .settings_dict import NamedSettingsDict  # noqa: F401
-from .settings_dict import DefaultSettingsDict  # noqa: F401
 from .view_buffer import ViewBuffer  # noqa: F401
 from .view_stream import ViewStream  # noqa: F401
 from .output_panel import OutputPanel  # noqa: F401
