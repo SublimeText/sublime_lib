13a4cc9ce9891b64676df4b8fd19dc031774f8cc
thom <thom@localhost.localdomain>
2019-01-04 13:53:37 -0500

    Added Panel class.

--- st3/sublime_lib/output_panel.py
+++ st3/sublime_lib/output_panel.py
@@ -1,12 +1,70 @@
 from .view_stream import ViewStream
 from .view_utils import set_view_options, validate_view_options
+from ._util.guard import define_guard
+
+__all__ = ['Panel', 'OutputPanel']
 
 
-__all__ = ['OutputPanel']
+class Panel():
+    """An abstraction of a panel, such as the console or an output panel.
+
+    :raise ValueError: if `window` has no panel called `panel_name`.
+
+    All :class:`Panel` methods except for :meth:`exists()`
+    will raise a ``ValueError`` if the panel does not exist.
+    Descendant classes may override :meth:`exists()` to customize this behavior.
+    """
+
+    def __init__(self, window, panel_name):
+        self.window = window
+        self.panel_name = panel_name
+
+        self._checkExists()
+
+    def _checkExists(self):
+        # if self.must_exist and not self.exists():
+        if not self.exists():
+            raise ValueError("Panel {} does not exist.".format(self.panel_name))
+
+    @define_guard
+    def guard_exists(self):
+        self._checkExists()
+
+    def exists(self):
+        """Return ``True`` if the panel exists, or ``False`` otherwise.
+
+        This implementation checks :meth:`sublime.Window.panels`,
+        so it will return ``False`` for unlisted panels.
+        """
+        return self.panel_name in self.window.panels()
+
+    @guard_exists
+    def is_visible(self):
+        """Return ``True`` if the panel is currently visible."""
+        return self.window.active_panel() == self.panel_name
+
+    @guard_exists
+    def show(self):
+        """Show the panel, hiding any other visible panel."""
+        self.window.run_command("show_panel", {"panel": self.panel_name})
+
+    @guard_exists
+    def hide(self):
+        """Hide the panel."""
+        self.window.run_command("hide_panel", {"panel": self.panel_name})
+
+    @guard_exists
+    def toggle_visibility(self):
+        """If the panel is visible, hide it; otherwise, show it."""
+        if self.is_visible():
+            self.hide()
+        else:
+            self.show()
 
 
-class OutputPanel(ViewStream):
-    """A :class:`~sublime_lib.view_stream.ViewStream`
+class OutputPanel(ViewStream, Panel):
+    """
+    A subclass of :class:`~sublime_lib.ViewStream` and :class:`~sublime_lib.Panel`
     wrapping an output panel in the given `window` with the given `name`.
 
     :raise ValueError: if `window` has no output panel called `name`.
@@ -42,9 +100,9 @@ class OutputPanel(ViewStream):
         if view is None:
             raise ValueError('Output panel "%s" does not exist.' % name)
 
-        super().__init__(view, force_writes=force_writes, follow_cursor=follow_cursor)
+        ViewStream.__init__(self, view, force_writes=force_writes, follow_cursor=follow_cursor)
+        Panel.__init__(self, window, "output." + name)
 
-        self.window = window
         self.name = name
 
     @property
@@ -54,30 +112,15 @@ class OutputPanel(ViewStream):
         Generally, API methods specific to output panels will use :attr:`name`,
         while methods that work with any panels will use :attr:`full_name`.
         """
-        return "output.%s" % self.name
+        return self.panel_name
 
-    @ViewStream.guard_validity
-    def is_visible(self):
-        """Return ``True`` if the output panel is currently visible."""
-        return self.window.active_panel() == self.full_name
+    def exists(self):
+        """Return ``True`` if the panel exists, or ``False`` otherwise.
 
-    @ViewStream.guard_validity
-    def show(self):
-        """Show the output panel, hiding any other visible panel."""
-        self.window.run_command("show_panel", {"panel": self.full_name})
-
-    @ViewStream.guard_validity
-    def hide(self):
-        """Hide the output panel."""
-        self.window.run_command("hide_panel", {"panel": self.full_name})
-
-    @ViewStream.guard_validity
-    def toggle_visibility(self):
-        """If the output panel is visible, hide it; otherwise, show it."""
-        if self.is_visible():
-            self.hide()
-        else:
-            self.show()
+        This implementation checks that the encapsulated :class:`~sublime.View` is valid,
+        so it will return ``True`` even for unlisted panels.
+        """
+        return self.view.is_valid()
 
     def destroy(self):
         """Destroy the output panel."""
--- st3/sublime_lib/view_stream.py
+++ st3/sublime_lib/view_stream.py
@@ -1,24 +1,9 @@
 from sublime import Region
 
 from contextlib import contextmanager
-from functools import wraps
 from io import SEEK_SET, SEEK_CUR, SEEK_END, TextIOBase
 
-
-def define_guard(guard_fn):
-    def decorator(wrapped):
-        @wraps(wrapped)
-        def wrapper_guards(self, *args, **kwargs):
-            ret_val = guard_fn(self)
-            if hasattr(ret_val, '__enter__'):
-                with ret_val:
-                    return wrapped(self, *args, **kwargs)
-            else:
-                return wrapped(self, *args, **kwargs)
-
-        return wrapper_guards
-
-    return decorator
+from ._util.guard import define_guard
 
 
 class ViewStream(TextIOBase):
