dbc75711b6ef9c47228c41f0ebcd4d460d40e233
Thomas Smith <thom@thomthesmith.com>
2019-05-25 14:43:31 -0400

    Improve/document LockedState.

--- st3/sublime_lib/_util/locked_state.py
+++ st3/sublime_lib/_util/locked_state.py
@@ -1,24 +1,31 @@
-from threading import Condition
+from threading import Condition, RLock
 
 from types import TracebackType
 from .._compat.typing import Optional, TypeVar, Generic, Callable
 
 T = TypeVar('T')
-R = TypeVar('R')
 
 __all__ = ['LockedState']
 
 
 class LockedState(Generic[T]):
+    """A value wrapped in a :class:`threading.Condition`.
+
+    The condition uses a reentrant lock,
+    so a thread may acquire it recursively.
+    """
     def __init__(self, initial_state: T) -> None:
         self._state = initial_state
-        self._condition = Condition()  # RLock
+        self._condition = Condition(RLock())
 
     @property
     def state(self) -> T:
+        """The current state."""
         return self._state
 
     def __enter__(self) -> T:
+        """Acquire the condition and return the current state.
+        """
         self._condition.acquire()
         return self.state
 
@@ -31,17 +38,23 @@ class LockedState(Generic[T]):
         self._condition.release()
 
     def set(self, state: T) -> None:
+        """Set the state, acquiring the condition temporarily."""
         with self._condition:
             self._state = state
             self._condition.notify_all()
 
     def wait_for(
         self,
-        predicate: Callable[[T], R],
+        predicate: Callable[[T], bool],
         timeout: Optional[float] = None
-    ) -> R:
+    ) -> bool:
+        """Wait until the state meets the given `predicate`.
+
+        Return ``True`` unless a given `timeout` expires,
+        in which case return ``False``.
+        """
         with self._condition:
-            return self._condition.wait_for(
+            return bool(self._condition.wait_for(
                 lambda: predicate(self.state),
                 timeout
-            )
+            ))
