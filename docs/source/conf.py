856cb811b5e5aaafb2a5aae19830bd11cbf3f7e3
Thom Smith <thom@thomthesmith.com>
2021-02-21 14:49:41 -0500

    Use sphinxcontrib.prettyspecialmethods instead of the original extension.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,6 +42,7 @@ release = ''
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
+    'sphinxcontrib.prettyspecialmethods',
 ]
 
 autodoc_member_order = 'bysource'
@@ -125,11 +126,6 @@ def setup(app):
     app.add_env_collector(TocTreeCollector)
 
     app.add_stylesheet('style.css')
-
-    from prettify_special_methods import PrettifySpecialMethods, show_special_methods
-    app.add_transform(PrettifySpecialMethods)
-
-    app.connect('autodoc-skip-member', show_special_methods)
 
     from strip_annotations import strip_annotations
     app.connect('autodoc-process-signature', strip_annotations)
