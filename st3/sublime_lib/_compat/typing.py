6c78be3dcc3339ab17dc59b38244c3d326ab8134
Thomas Smith <thom@thomthesmith.com>
2021-07-04 21:50:41 -0400

    Change typing stub to make pyright happy. (#172)

--- st3/sublime_lib/_compat/typing.py
+++ st3/sublime_lib/_compat/typing.py
@@ -1,4 +1,6 @@
-try:
+import sys
+
+if sys.version_info >= (3, 8, 0):
     from typing import *  # noqa: F401, F403
-except ImportError:
+else:
     from .typing_stubs import *  # type: ignore # noqa: F401, F403
