7a9eba3b25bb54eaac244319b748d187e27297bf
deathaxe <deathaxe82@gmail.com>
2026-01-11 22:49:47 +0100

    Remove py38 incompatible API calls
    Plump print as warnings.deprecated is not supported by python 3.8.

--- sublime_lib/syntax.py
+++ sublime_lib/syntax.py
@@ -1,22 +1,18 @@
 from __future__ import annotations
-from warnings import deprecated
-from typing import TypeAlias
 
 import sublime
 
 __all__ = ['list_syntaxes', 'get_syntax_for_scope']
 
 
-@deprecated(
-    "sublime_lib.list_syntaxes() is deprecated, use sublime.list_syntaxes() instead!"
-)
 def list_syntaxes() -> list[sublime.Syntax]:
+    print("sublime_lib.list_syntaxes() is deprecated,"
+          " use sublime.list_syntaxes() instead!")
     return sublime.list_syntaxes()
 
 
-@deprecated(
-    "sublime_lib.get_syntax_for_scope() is deprecated, use sublime.find_syntax_by_scope() instead!"
-)
 def get_syntax_for_scope(scope: str) -> str:
+    print("sublime_lib.get_syntax_for_scope() is deprecated,"
+          " use sublime.find_syntax_by_scope() instead!")
     syntax = sublime.find_syntax_by_scope(scope)
     return syntax[0].path if syntax else ""
